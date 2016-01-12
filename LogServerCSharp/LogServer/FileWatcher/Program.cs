d462a391d628bffecc2b33f66f25422ed91abc4a
GWigWam <welwisam@gmail.com>
2016-01-12 17:51:54 +0100

    Print random entry on 'S' stat
    Gives some insight in the current data

--- LogServerCSharp/LogServer/FileWatcher/Program.cs
+++ LogServerCSharp/LogServer/FileWatcher/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 namespace FileWatcher {
 
     internal class Program {
-        private static DataAccess DataAcc;
+        private static DataAccess DataObj;
         private const string ConfigLogFolderKey = "LogFilesFolder";
 
         private static void Main(string[] args) {
@@ -20,9 +20,9 @@ namespace FileWatcher {
             string logFolderPath = string.Empty;
             if(settings.AllKeys.Contains(ConfigLogFolderKey) && Directory.Exists(logFolderPath = settings[ConfigLogFolderKey].Value)) {
                 Log($"Log folder set to: '{logFolderPath}'");
-                DataAcc = new DataAccess();
+                DataObj = new DataAccess();
 
-                var lfu = new LogfileUpdate(logFolderPath, DataAcc);
+                var lfu = new LogfileUpdate(logFolderPath, DataObj);
                 lfu.Log += Log;
                 lfu.Init();
             } else {
@@ -32,11 +32,11 @@ namespace FileWatcher {
             IPLocate.Init();
             Log($"Read IP locations");
 
-            foreach(var entry in DataAcc.Context.LogEntries.Where(le => le.Country == null)) {
+            foreach(var entry in DataObj.Context.LogEntries.Where(le => le.Country == null)) {
                 var calced = IPLocate.GetIpCountry(entry.RemoteIp);
                 entry.Country = calced;
             }
-            DataAcc.SaveChanges();
+            DataObj.SaveChanges();
 
             PrintHelp();
             bool running = true;
@@ -48,7 +48,7 @@ namespace FileWatcher {
                     PrintHelp();
                     break;
                     case ConsoleKey.C:
-                    DataAcc.Clear();
+                    DataObj.Clear();
                     Log("Cleared DB");
                     break;
                     case ConsoleKey.S:
@@ -78,8 +78,21 @@ namespace FileWatcher {
         }
 
         private static void PrintStats() {
+            var logCount = DataObj.Context.LogEntries.Count();
             Console.WriteLine("\nStats:");
-            Console.WriteLine($"ReadFiles: {DataAcc.Context.ReadFiles.Count()}\nLogEntries: {DataAcc.Context.LogEntries.Count()}\n");
+            Console.WriteLine($"ReadFiles: {DataObj.Context.ReadFiles.Count()}\nLogEntries: {logCount}\n");
+
+            if(logCount > 0) {
+                var r = new Random().Next(0, logCount - 1);
+                var entry = DataObj.Context.LogEntries.OrderBy(le => le.ID).Skip(r).First();
+                Console.WriteLine($"Random (#{r}) entry:");
+                foreach(var prop in typeof(Data.Models.LogEntry).GetProperties()) {
+                    var val = prop.GetValue(entry).ToString();
+                    if(val.Length > 30)
+                        val = val.Substring(0, 29);
+                    Console.WriteLine($"{prop.Name}: {val}");
+                }
+            }
         }
 
         private static void Log(string msg, ConsoleColor? ForeColor = null, ConsoleColor? BackColor = null) {
